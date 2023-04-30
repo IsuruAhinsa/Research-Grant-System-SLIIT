16c2275aafa82de50813145a70505149299dfb44
IsuruAhinsa <isurujayawickramat@gmail.com>
2023-04-30 16:46:51 +0530

    Breadcrumb Configured

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,18 @@
+<?php
+
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+Breadcrumbs::for('dashboard', function (BreadcrumbTrail $trail) {
+    $trail->push('Dashboard', route('dashboard'));
+});
+
+Breadcrumbs::for('users.index', function (BreadcrumbTrail $trail) {
+    $trail->parent('dashboard');
+    $trail->push('Users', route('users.index'));
+});
+
+Breadcrumbs::for('users.trash', function (BreadcrumbTrail $trail) {
+    $trail->parent('dashboard');
+    $trail->push('Deleted Users', route('users.trash'));
+});
