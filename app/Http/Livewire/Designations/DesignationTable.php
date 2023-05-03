805cef06560cc77d71089b6c05a82b6dfe86ff2c
IsuruAhinsa <isurujayawickramat@gmail.com>
2023-05-03 19:06:36 +0530

    added bulk actions, implement delete function

--- app/Http/Livewire/Designations/DesignationTable.php
+++ app/Http/Livewire/Designations/DesignationTable.php
@@ -4,8 +4,12 @@ namespace App\Http\Livewire\Designations;
 
 use App\Models\Designation;
 use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Livewire\Component;
 use Filament\Tables;
 
@@ -29,21 +33,29 @@ class DesignationTable extends Component implements Tables\Contracts\HasTable
     protected function getTableActions(): array
     {
         return [
-            Action::make('edit')
-                ->url(fn(Designation $record): string => route('designations.edit', $record))
-                ->icon('heroicon-s-pencil')
-                ->label('Edit')
-                ->color('success'),
-
-            Action::make('delete')
-//                ->url(fn(Role $record): string => route('roles.destroy', $record))
-                ->icon('heroicon-s-trash')
-                ->label('Delete')
-                ->color('danger')
-            // TODO: delete action implement
+            ActionGroup::make([
+                Action::make('edit')
+                    ->url(fn(Designation $record): string => route('designations.edit', $record))
+                    ->icon('heroicon-s-pencil')
+                    ->label('Edit')
+                    ->color('primary'),
+                DeleteAction::make()->modalButton('Delete')
+            ])
         ];
     }
 
+    protected function getTableBulkActions(): array
+    {
+        return [
+            BulkAction::make('delete')
+                ->action(fn(Collection $records) => $records->each->delete())
+                ->deselectRecordsAfterCompletion()
+                ->requiresConfirmation()
+                ->modalHeading('Delete Designations')
+                ->modalSubheading('Are you sure you\'d like to delete these designations? This cannot be undone.')
+                ->modalButton('Yes, delete them')
+        ];
+    }
 
     public function render()
     {
