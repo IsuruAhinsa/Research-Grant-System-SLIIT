74ffb580ed97619041f1f15c28a699b77ee8a3a9
IsuruAhinsa <isurujayawickramat@gmail.com>
2023-05-03 19:04:31 +0530

    added bulk actions, implement delete function

--- app/Http/Livewire/Faculties/FacultyTable.php
+++ app/Http/Livewire/Faculties/FacultyTable.php
@@ -4,8 +4,12 @@ namespace App\Http\Livewire\Faculties;
 
 use App\Models\Faculty;
 use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Livewire\Component;
 use Filament\Tables;
 
@@ -30,20 +34,30 @@ class FacultyTable extends Component implements Tables\Contracts\HasTable
     protected function getTableActions(): array
     {
         return [
-            Action::make('edit')
-                ->url(fn(Faculty $record): string => route('faculties.edit', $record))
-                ->icon('heroicon-s-pencil')
-                ->label('Edit')
-                ->color('success'),
-
-            Action::make('delete')
-//                ->url(fn(Role $record): string => route('roles.destroy', $record))
-                ->icon('heroicon-s-trash')
-                ->label('Delete')
-                ->color('danger')
-            // TODO: delete action implement
+            ActionGroup::make([
+                Action::make('edit')
+                    ->url(fn(Faculty $record): string => route('faculties.edit', $record))
+                    ->icon('heroicon-s-pencil')
+                    ->label('Edit')
+                    ->color('primary'),
+                DeleteAction::make()->modalButton('Delete')
+            ])
         ];
     }
+
+    protected function getTableBulkActions(): array
+    {
+        return [
+            BulkAction::make('delete')
+                ->action(fn(Collection $records) => $records->each->delete())
+                ->deselectRecordsAfterCompletion()
+                ->requiresConfirmation()
+                ->modalHeading('Delete Faculties')
+                ->modalSubheading('Are you sure you\'d like to delete these faculties? This cannot be undone.')
+                ->modalButton('Yes, delete them')
+        ];
+    }
+
     public function render()
     {
         return <<<'blade'
