7473076eab070c6aecb2ca4637d9a958a8848273
IsuruAhinsa <isurujayawickramat@gmail.com>
2023-05-09 21:29:58 +0530

    Add badge as a component

---
+++ resources/views/components/ui/badge.blade.php
@@ -0,0 +1,4 @@
+<div
+    {!! $attributes->merge(['class' => 'inline-flex items-center justify-center space-x-1 rtl:space-x-reverse min-h-6 px-2 py-0.5 text-sm font-medium tracking-tight rounded-xl whitespace-nowrap text-warning-700 bg-warning-500/10']) !!}>
+    {{$slot}}
+</div>
