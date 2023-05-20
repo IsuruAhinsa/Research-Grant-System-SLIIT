b353dafd3b8200f11b978a73a457bbb90d40582e
IsuruAhinsa <isurujayawickramat@gmail.com>
2023-05-20 13:15:41 +0530

    display role name in navigation menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -82,8 +82,7 @@
                             @else
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-primary-900 hover:text-gray-200 focus:outline-none transition ease-in-out duration-150">
-                                        {{ Auth::user()->fullname }}
-
+                                        {{ Auth::user()->getRoleNames()->first() }}
                                         <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                         </svg>
@@ -94,8 +93,9 @@
 
                         <x-slot name="content">
                             <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
+                            <div class="block px-4 py-2 text-gray-400">
+                                <span class="text-xs">{{ __('Manage Account') }}</span>
+                                <p class="text-black">{{ Auth::user()->fullname }}</p>
                             </div>
 
                             <x-ui.dropdown-link href="{{ route('profile.show') }}">
