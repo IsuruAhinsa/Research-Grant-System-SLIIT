7a9be970e4da0603f373808c4333b7fe3b4466be
IsuruAhinsa <isurujayawickramat@gmail.com>
2023-05-25 12:41:04 +0530

    factories updated

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,8 +31,8 @@ class UserFactory extends Factory
             'title' => $this->faker->title,
             'first_name' => $this->faker->firstName,
             'last_name' => $this->faker->lastName,
-            'email' => $this->faker->userName . '@' . 'sliit.lk',
-            'index' => $this->faker->randomNumber(4, true),
+            'email' => $this->faker->unique()->userName . '@' . 'sliit.lk',
+            'index' => $this->faker->unique()->randomNumber(4, true),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'two_factor_secret' => null,
