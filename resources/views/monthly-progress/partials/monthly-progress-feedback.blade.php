299faef16e64d5842aec58ff691e8569a5d5f656
IsuruAhinsa <isurujayawickramat@gmail.com>
2023-07-03 10:34:25 +0530

    spelling fixed

--- resources/views/monthly-progress/partials/monthly-progress-feedback.blade.php
+++ resources/views/monthly-progress/partials/monthly-progress-feedback.blade.php
@@ -1,7 +1,7 @@
 @if($monthlyProgress->checkExistsGrading())
 
     <div class="mt-8 flex flex-col">
-        <h2 class="text-2xl mb-2">Monthly Progress Grading Summery</h2>
+        <h2 class="text-2xl mb-2">Monthly Progress Grading Summary</h2>
         <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                 <div class="overflow-hidden border-4 border-dashed border-yellow-500 md:rounded-lg">
